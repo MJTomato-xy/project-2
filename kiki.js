d083947b7450db5d99bf11926efcb51637fe77e3
Xinyi-Liu-024 <59549169+Xinyi-Liu-024@users.noreply.github.com>
2020-01-21 17:00:19 -0500

    first commit

---
+++ kiki.js
@@ -0,0 +1,8 @@
+<script>
+var element = document.querySelector(".door");
+element.addEventListener("click", toggleDoor);
+
+function toggleDoor() {
+  element.classList.toggle("doorOpen");
+}
+</script>
