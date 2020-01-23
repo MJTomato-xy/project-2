b02e66fac4c51f81afb0369242ca6af0da36f923
Xinyi-Liu-024 <59549169+Xinyi-Liu-024@users.noreply.github.com>
2020-01-23 15:12:21 -0500

    width change

--- kiki.js
+++
@@ -1,8 +0,0 @@
-<script>
-var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
-
-function toggleDoor() {
-  element.classList.toggle("doorOpen");
-}
-</script>
